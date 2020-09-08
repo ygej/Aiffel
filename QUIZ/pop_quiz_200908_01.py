6988323646a90f0c5aa862a78946e6bc2382bc62
ygej <plmng12@gmail.com>
2020-09-08 20:30:38 +0900

    edit quiz 20200908

--- QUIZ/pop_quiz_200908_01.py
+++ QUIZ/pop_quiz_200908_01.py
@@ -8,18 +8,28 @@
 # 예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
 # 앞으로 정답의 형태는 오늘 배운 함수로!! 진행해주시면 됩니다. 예를 들어 위의 문제 같은 경우는 입력값을 a, b로 받고 리턴은 12를 한다했으므로
 
-# In[1]:
+# In[7]:
 
 
 def solution(a,b):
     answer = 0
-    for i in range (a,b+1):
-        answer += i
+    if a >= b:
+        for i in range(b,a+1):
+            answer += i
+    elif b >= a:
+        for i in range(a,b+1):
+            answer += i
     return answer
 
 
-# In[2]:
+# In[11]:
 
 
-solution(3,5)
+solution(21,-1)
+
+
+# In[ ]:
+
+
+
 
