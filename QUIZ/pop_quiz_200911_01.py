6d6be3df18b009078339c4e991ce4fd7e91dc1cb
ygej <plmng12@gmail.com>
2020-09-11 19:38:31 +0900

    upload pop quiz 200911 01

---
+++ QUIZ/pop_quiz_200911_01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 깜퀴_20200911
+
+# # 문제 1
+# 구구단 만들기
+
+# In[4]:
+
+
+for i in range(1,10):
+    for j in range(1,10):
+        print(i,'*',j, '=', i*j)
+
